d58611ba80b2bb1d9c038205ed1867e3ae730ce2
AmandaYang <yuanyuan.yang@alumni.ubc.ca>
2021-08-31 16:51:53 -0700

    complete week4 assignment

--- src/module4/OceanQuakeMarker.java
+++ src/module4/OceanQuakeMarker.java
@@ -26,15 +26,14 @@ public class OceanQuakeMarker extends EarthquakeMarker {
 		// class to indicate the depth of the earthquake.
 		// Simply draw a centered square.
 		
-		// HINT: Notice the radius variable in the EarthquakeMarker class
-		// and how it is set in the EarthquakeMarker constructor
+		pg.pushStyle();
 		
-		// TODO: Implement this method
+		pg.rect(x, y, getRadius(), getRadius());
+		
+		pg.popStyle();
 		
 	}
 	
-
-
 	
 
 }
